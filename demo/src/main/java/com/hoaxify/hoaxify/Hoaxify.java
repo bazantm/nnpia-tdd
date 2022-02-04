7791c05d7b40ebdc6fd1d8972b60462aac35ce94
Michael Bazant <bazantm@gmail.com>
2022-02-04 16:58:22 +0100

    init nnpia-tdd

---
+++ demo/src/main/java/com/hoaxify/hoaxify/Hoaxify.java
@@ -0,0 +1,13 @@
+package com.hoaxify.hoaxify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoaxify {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hoaxify.class, args);
+	}
+
+}
